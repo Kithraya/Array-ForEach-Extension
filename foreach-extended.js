cf78184fc862e8addd65a338c82f475b866612e5
Kithraya <46088893+Kithraya@users.noreply.github.com>
2020-08-02 05:25:22 +0100

    Update foreach-extended.js

--- foreach-extended.js
+++ foreach-extended.js
@@ -2,8 +2,8 @@ foreach.version = "1.2.3";
 
 function foreach(array, callback, dynamiclength, callscope) { 
 
-	if (!array) { return } // Return `undefined` if falsy. It's not necessarily an array, but just needs to be iterable in some form. 
-	// At present, `undefined`, `false`, `0`, `null`, `NaN`, and `''` do not make sense being iterable.
+	if (!array) { return } // Return `undefined` if falsy. It's not necessarily an array, it does need to be iterable in some form. 
+	// At present, `undefined`, `false`, `0`, `null`, `NaN`, and `''` do not make sense being iterable values.
 	if (typeof callback !== 'function') { throw new TypeError(callback + ' is not a function!'); }
 	
 	var i=0;
